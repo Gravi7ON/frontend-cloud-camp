b252454d0618028d64c3b1d08f0c25642adbb9d2
Dmitry Radionov <radionovdmr@gmail.com>
2023-06-16 19:58:36 +0500

    Подготавливает необходимые ui компоненты

--- src/components/step-two-form/step-two-form.tsx
+++ src/components/step-two-form/step-two-form.tsx
@@ -87,65 +87,65 @@ export default function StepTwoForm(): JSX.Element {
           </svg>
         </div>
         <div className="popup-form__form-step-one">
-          <form>
-            <label>
-              <span className="input__label">Nickname</span>
-              <input
-                id="field-nickname"
-                className="step-one"
-                name="nickname"
-                placeholder="Placeholder"
-              />
-              <span className="input-error_message"></span>
+          <form style={{ width: '336px' }}>
+            <label htmlFor="advantages-list">
+              <span className="input__label">Advantages</span>
             </label>
-            <label>
-              <span className="input__label">Name</span>
-              <input
-                id="field-name"
-                className="step-one"
-                name="name"
-                placeholder="Placeholder"
-              />
-              <span className="input-error_message"></span>
-            </label>
-            <label>
-              <span className="input__label">Surname</span>
-              <input
-                id="field-surname"
-                className="step-one"
-                name="surmane"
-                placeholder="Placeholder"
-              />
-              <span className="input-error_message"></span>
-            </label>
-            <label>
-              <span className="input__label">Sex</span>
-              <select
-                id="field-sex"
-                className="step-one"
-                name="sex"
-              >
-                <option
-                  selected
-                  disabled
+            <ul id="advantages-list">
+              <li className="advantages-list__container">
+                <input
+                  id="field-advantages-1"
+                  className="step-one"
+                  name="advantages-1"
+                  placeholder="Placeholder"
+                />
+                <svg
+                  className="input-trash__remove"
+                  id="button-remove-1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="19"
+                  viewBox="0 0 20 19"
+                  fill="none"
                 >
-                  Не выбрано
-                </option>
-                <option
-                  id="field-sex-option-man"
-                  value="man"
-                >
-                  man
-                </option>
-                <option
-                  id="field-sex-option-woman"
-                  value="woman"
-                >
-                  woman
-                </option>
-              </select>
-              <span className="input-error_message"></span>
+                  <path
+                    d="M5.453 16.6522L4.55826 8.15225C4.52719 7.85703 4.75867 7.5999 5.05552 7.5999H14.9447C15.2416 7.5999 15.4731 7.85703 15.442 8.15225L14.5472 16.6522C14.5205 16.9067 14.3059 17.0999 14.05 17.0999H5.95025C5.69437 17.0999 5.47979 16.9067 5.453 16.6522Z"
+                    fill="#CCCCCC"
+                  />
+                  <path
+                    d="M17.0001 5.6999H3.00012C2.72398 5.6999 2.50012 5.47605 2.50012 5.1999V4.2999C2.50012 4.02376 2.72398 3.7999 3.00012 3.7999H5.35511C5.44983 3.7999 5.54261 3.77299 5.62263 3.72231L8.37761 1.97749C8.45764 1.92681 8.55041 1.8999 8.64514 1.8999H11.3551C11.4498 1.8999 11.5426 1.92681 11.6226 1.97749L14.3776 3.72231C14.4576 3.77299 14.5504 3.7999 14.6451 3.7999H17.0001C17.2763 3.7999 17.5001 4.02376 17.5001 4.2999V5.1999C17.5001 5.47604 17.2763 5.6999 17.0001 5.6999Z"
+                    fill="#CCCCCC"
+                  />
+                </svg>
+              </li>
+            </ul>
+            <button id="button-add">+</button>
+            <label htmlFor="checkbox-list">
+              <span className="input__label">Checkbox groupe</span>
             </label>
+            <ul id="checkbox-list">
+              <li className="checkbox-list-container">
+                <input
+                  className="field-checkbox-groupe-option"
+                  id="field-checkbox-groupe-option-1"
+                  type="checkbox"
+                />
+                <span>1</span>
+              </li>
+            </ul>
+            <label htmlFor="radio-button-list">
+              <span className="input__label">Checkbox groupe</span>
+            </label>
+            <ul id="radio-button-list">
+              <li className="checkbox-list-container">
+                <input
+                  className="field-checkbox-groupe-option"
+                  id="field-checkbox-groupe-option-1"
+                  type="radio"
+                />
+                <span>1</span>
+              </li>
+            </ul>
           </form>
         </div>
         <div className="button-container">
